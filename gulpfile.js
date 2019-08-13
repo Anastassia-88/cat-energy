1cc692e2c072763a698ed77ee52881444abe5fff
Anastassia-88 <anastassia.russak@gmail.com>
2019-08-13 20:22:20 +0200

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,41 @@ var jsmin = require("gulp-uglify");
 var del = require("del");
 var server = require("browser-sync").create();
 
+//  Removes all files in the "build" folder
+gulp.task("clean", function () {
+  return del("build");
+});
+
+//  Copies files from the "source" folder to the "build" folder
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+//  Converts images in the "source" folder to WebP (npx gulp webp)
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+//  Combines svg files into one with <symbol> elements
+gulp.task("sprite", function () {
+  return gulp.src("source/img/sprite/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+//  Prefixes CSS properties
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -31,8 +66,17 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
+gulp.task("normalize", function () {
+  return gulp.src("source/css/normalize.css")
+    .pipe(plumber())
+    .pipe(csso())
+    .pipe(rename("normalize.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
 gulp.task("images", function () {
-  return gulp.src("source/img/../*{.png,jpg,svg}")
+  return gulp.src("source/img/**/*{.png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
@@ -41,21 +85,7 @@ gulp.task("images", function () {
     .pipe(gulp.dest("source/img"));
 });
 
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
-});
-
-gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
-});
-
+//  Minifies HTML
 gulp.task("html", function () {
   return gulp.src("source/*.html")
     .pipe(posthtml([
@@ -65,32 +95,20 @@ gulp.task("html", function () {
     .pipe(gulp.dest("build"));
 });
 
+//  Minifies JavaScript
 gulp.task("js", function () {
   return gulp.src("source/js/*.js")
     .pipe(jsmin())
-    .pipe(gulp.dest("source/js"));
-});
-
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source/*.ico"
-  ], {
-    base: "source"
-  })
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function () {
-  return del("build");
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest("build/js"))
+    .pipe(server.stream());
 });
 
 gulp.task("build", gulp.series(
   "clean",
   "copy",
   "css",
+  "normalize",
   "sprite",
   "html",
   "js"
@@ -104,6 +122,7 @@ gulp.task("server", function () {
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js"));
 });
 
 gulp.task("refresh", function (done) {
--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,100 @@
-var navMain=document.querySelector(".main-nav"),navToggle=navMain.querySelector(".main-nav__toggle");navMain.classList.remove("main-nav--nojs"),navToggle.addEventListener("click",function(){navMain.classList.contains("main-nav--closed")?(navMain.classList.remove("main-nav--closed"),navMain.classList.add("main-nav--opened")):(navMain.classList.add("main-nav--closed"),navMain.classList.remove("main-nav--opened"))});var toggleContainer=document.querySelector(".example__toggle-container"),imageBefore=document.querySelector(".example__image--before"),imageAfter=document.querySelector(".example__image--after"),controlBefore=document.querySelector(".example__control-before"),controlAfter=document.querySelector(".example__control-after");toggleContainer.addEventListener("click",function(){imageBefore.classList.toggle("hide"),imageAfter.classList.toggle("hide"),controlBefore.classList.toggle("hide"),controlAfter.classList.toggle("hide")});
+"use strict";
+
+// Main Navigation
+var navMain = document.querySelector(".main-nav");
+var navToggle = navMain.querySelector(".main-nav__toggle");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener('click', function() {
+  navMain.classList.toggle('main-nav--closed');
+  navMain.classList.toggle('main-nav--opened');
+});
+
+
+
+// Before - After
+var controlContainer = document.querySelector(".example__control-container");
+var imageContainer = document.querySelector(".example__image-container");
+var imageBefore = document.querySelector(".picture__image--before");
+var imageAfter = document.querySelector(".picture__image--after");
+var control = document.querySelector(".example__control");
+var textBefore = document.querySelector(".example__control-text--before");
+var textAfter = document.querySelector(".example__control-text--after");
+var flag = false;
+var exampleControls = document.querySelector(".example__controls");
+
+// controlContainer.addEventListener("click", function () {
+//   imageBefore.classList.toggle("hide");
+//   imageAfter.classList.toggle("hide");
+//   control.classList.toggle("example__control--after");
+// });
+//
+// textBefore.addEventListener("click", function () {
+//   if (imageBefore.classList.contains("hide")) {
+//     imageBefore.classList.remove("hide");
+//     imageAfter.classList.add("hide");
+//     control.classList.remove("example__control--after");
+//   }
+// });
+//
+// textAfter.addEventListener("click", function () {
+//   if (imageAfter.classList.contains("hide")) {
+//     imageBefore.classList.add("hide");
+//     imageAfter.classList.remove("hide");
+//     control.classList.add("example__control--after");
+//   }
+// });
+
+control.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  flag = true;
+  console.log("mousedown");
+  console.log(flag);
+}, false);
+
+document.addEventListener('mouseup', function () {
+  flag = false;
+  console.log("mouseup");
+  console.log(flag);
+}, false);
+
+exampleControls.addEventListener('mousemove', function (evt) {
+  var res = evt.pageX - this.offsetLeft - controlContainer.offsetLeft - controlContainer.offsetWidth / 2;
+
+  if (flag && evt.pageX > 0) {
+    control.style.left = (controlContainer.offsetWidth / 2 + res) + "px";
+  }
+  else if (flag && evt.pageX < 0) {
+    control.style.left = (controlContainer.offsetWidth / 2 - res) + "px";
+  }
+
+
+
+
+  // control.style.left = evt.pageX + "px";
+
+
+
+  // var res = evt.pageX - control.offsetLeft;
+  //
+  // if ((res > 0) && (res < imageContainer.offsetWidth)) {
+  //   control.style.left += res;
+  //   imageBefore.style.width = res + 'px';
+  //   imageAfter.style.width = res + 'px';
+  // }
+
+}, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
