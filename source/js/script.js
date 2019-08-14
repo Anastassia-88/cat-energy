a3c83cde75834fefc02bda567a27d1de597a5357
Anastassia-88 <anastassia.russak@gmail.com>
2019-08-14 07:38:52 +0200

    Доделываю котов

--- source/js/script.js
+++ source/js/script.js
@@ -23,28 +23,22 @@ var textBefore = document.querySelector(".example__control-text--before");
 var textAfter = document.querySelector(".example__control-text--after");
 var flag = false;
 var exampleControls = document.querySelector(".example__controls");
-
+//
 // controlContainer.addEventListener("click", function () {
 //   imageBefore.classList.toggle("hide");
 //   imageAfter.classList.toggle("hide");
 //   control.classList.toggle("example__control--after");
 // });
-//
-// textBefore.addEventListener("click", function () {
-//   if (imageBefore.classList.contains("hide")) {
-//     imageBefore.classList.remove("hide");
-//     imageAfter.classList.add("hide");
-//     control.classList.remove("example__control--after");
-//   }
-// });
-//
-// textAfter.addEventListener("click", function () {
-//   if (imageAfter.classList.contains("hide")) {
-//     imageBefore.classList.add("hide");
-//     imageAfter.classList.remove("hide");
-//     control.classList.add("example__control--after");
-//   }
-// });
+
+textBefore.addEventListener("click", function () {
+pictureBefore.style.width = 100 + "%";
+pictureAfter.style.width = 0 + "%";
+});
+
+textAfter.addEventListener("click", function () {
+pictureBefore.style.width = 0 + "%";
+pictureAfter.style.width = 100 + "%";
+});
 
 control.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
